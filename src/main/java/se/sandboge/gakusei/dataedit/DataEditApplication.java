c75740fd0d7040a83e02318e88490a969f20a5d1
Pierre Sandboge <pierre@sandboge.com>
2017-04-05 09:07:13 +0200

    Initial commit

---
+++ src/main/java/se/sandboge/gakusei/dataedit/DataEditApplication.java
@@ -0,0 +1,11 @@
+package se.sandboge.gakusei.dataedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataEditApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataEditApplication.class, args);
+    }
+}
